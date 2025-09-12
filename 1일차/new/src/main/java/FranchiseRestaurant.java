3a554982ca987b4cc048a69ba8b1532db1301df9
soyeon911 <sypark911@yonsei.ac.kr>
2025-09-13 02:50:29 +0900

    Franchise restaurant class

--- 1일차/new/src/main/java/FranchiseRestaurant.java
+++ 1일차/new/src/main/java/FranchiseRestaurant.java
@@ -1,3 +1,5 @@
+// 레스토랑 하위 클래스의 프랜차이즈 레스토랑 클래스
+// 레스토랑 클래스와 달리 프렌차이즈 이름, 지점명 속성 추가된 ver.
 public class FranchiseRestaurant extends Restaurant{
     private String brandName;
     private String branchCode;
