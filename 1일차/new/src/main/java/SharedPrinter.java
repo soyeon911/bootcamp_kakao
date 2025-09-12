041ed83ae4293f0115ea33c1db97f906fae58c80
soyeon911 <sypark911@yonsei.ac.kr>
2025-09-13 02:56:25 +0900

    synchronized로 모니터 락 구현

--- 1일차/new/src/main/java/SharedPrinter.java
+++ 1일차/new/src/main/java/SharedPrinter.java
@@ -1,26 +1,15 @@
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+// monitor lock(Synchronized 사용)
+// 이 객체(SharedPrinter)를 모니터 락으로 사용하자
+// 한 번에 하나의 쓰레드만 통과
 
 public class SharedPrinter {
-    private final Lock lock = new ReentrantLock();
-
-    public void print(String s) {
-        lock.lock();
-        try {
-            System.out.print(s);
-            System.out.flush();
-        } finally {
-            lock.unlock();
-        }
+    // synchronized 메서드 사용 -> 같은 객체에 대해 한 번에 하나의 스레드만 접근 가능함
+    public synchronized void print(String s){
+        System.out.println(s);
+        System.out.flush();     // 버퍼를 비워서 즉시 출력하게 함
     }
-
-    public void println(String s) {
-        lock.lock();
-        try {
-            System.out.println(s);
-            System.out.flush();
-        } finally {
-            lock.unlock();
-        }
+    public synchronized void println(String s){
+        System.out.println(s);
+        System.out.flush();
     }
 }
